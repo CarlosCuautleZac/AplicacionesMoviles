14ff6a177deca7f412e2958fe04ffee4e9cfba7f
Procaronte <sirmisaelcr@hotmail.com>
2023-02-03 15:07:38 -0600

    Proyecto maui

---
+++ PendientesApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PendientesApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
