559ca6cc452690613ae1e816de4369edf197e0df
Procaronte <sirmisaelcr@hotmail.com>
2023-02-16 15:01:02 -0600

    Repository e inyeccion

--- AerolineaTECAPI/Models/Sistem21AerolineadbContext.cs
+++ AerolineaTECAPI/Models/Sistem21AerolineadbContext.cs
@@ -17,9 +17,7 @@ public partial class Sistem21AerolineadbContext : DbContext
 
     public virtual DbSet<Vuelo> Vuelo { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=sistemas19.com;user=sistem21_AerolineaDB;password=sistemas19_;database=sistem21_aerolineadb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.5.17-mariadb"));
+   
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- AerolineaTECAPI/Repositories/Repository.cs
+++ AerolineaTECAPI/Repositories/Repository.cs
@@ -1,6 +1,37 @@
-﻿namespace AerolineaTECAPI.Repositories
+﻿using AerolineaTECAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AerolineaTECAPI.Repositories
 {
-    public class Repository
+    public class Repository<T> where T:class 
     {
+        private readonly Sistem21AerolineadbContext context;
+        public Repository(Sistem21AerolineadbContext context)
+        {
+            this.context = context;
+        }
+        public DbSet<T> Get()
+        {
+            return context.Set<T>();
+        }
+        public T? Get(object id)
+        {
+            return context.Find<T>(id);
+        }
+        public void Insert(T entity)
+        {
+            context.Add(entity);
+            context.SaveChanges();
+        }
+        public void Update(T entity)
+        {
+            context.Update(entity);
+            context.SaveChanges();
+        }
+        public void Delete(T entity)
+        {
+            context.Remove(entity);
+            context.SaveChanges();
+        }
     }
 }
