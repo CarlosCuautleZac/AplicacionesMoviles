3f192de0b95002e8df2468483b2cd6a9e93c21b1
operador <operador@CCA5-07>
2023-03-13 14:57:43 -0600

    sqlite-net-pcl packet- install ,repository vuelo,services vuelo get, helpersdatabase

---
+++ PantallaVuelosMAUI/Repositories/VuelosRepository.cs
@@ -0,0 +1,46 @@
+﻿using PantallaVuelosMAUI.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PantallaVuelosMAUI.Repositories
+{
+    public class VuelosRepository
+    {
+        SQLiteConnection context;
+
+        public VuelosRepository()
+        {
+            context = new SQLiteConnection(Helpers.DatabaseHelper.RutaBD);
+            context.CreateTable<Vuelo>();
+        }
+
+        public List<Vuelo> GetAll()
+        {
+            return new List<Vuelo>();
+        }
+
+        public List<Vuelo> Get(int id)
+        {
+            return new List<Vuelo>();
+        }
+
+        public void Insert(Vuelo v)
+        {
+            context.Insert(v);
+        }
+
+        public void Update(Vuelo v)
+        {
+            context.Update(v);
+        }
+
+        public void Delete(int id)
+        {
+            context.Delete<Vuelo>(id);
+        }
+    }
+}
---
+++ PantallaVuelosMAUI/Services/VueloService.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using PantallaVuelosMAUI.Models;
+using PantallaVuelosMAUI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Windows.Media.Protection.PlayReady;
+
+namespace PantallaVuelosMAUI.Services
+{
+    public class VueloService
+    {
+        HttpClient client;
+        VuelosRepository repository = new VuelosRepository();
+
+        //Sincronizador de microservicios
+        public VueloService()
+        {
+            client.BaseAddress = new Uri("https://aerolineatec.sistemas19.com/");
+        }
+
+        public async Task<IEnumerable<Vuelo>> GetAsync()
+        {
+            if (Connectivity.Current.NetworkAccess == NetworkAccess.Internet)
+            {
+                //Descargar de la api
+                var response = await client.GetAsync("api/vuelos");
+                response.EnsureSuccessStatusCode();
+                var json = await response.Content.ReadAsStringAsync();
+                var vuelos = JsonConvert.DeserializeObject<List<Vuelo>>(json);
+                
+
+                //Actualizacion en la BD local
+                foreach (var item in vuelos)
+                {
+                    var vuelo = repository.Get(item.Id);
+                    if (vuelo == null)
+                    {
+                        repository.Insert(item);
+                    }
+                    else
+                        repository.Update(item);
+                }
+
+                foreach (var item in repository.GetAll())
+                {
+                    if (!vuelos.Any(x => x.Id == item.Id))
+                    {
+                        repository.Delete(item.Id);
+                    }
+                }
+            }
+
+            return repository.GetAll();
+
+        }
+    }
+
+
+}
