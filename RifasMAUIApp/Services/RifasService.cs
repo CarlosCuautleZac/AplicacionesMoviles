b38f2fffe096a68a7d31e22066ae64e912e1c2db
CarlosCuautleZac <191G0235@rcarbonifera.tecnm.mx>
2023-04-17 15:52:15 -0500

    Creacion de servicios

---
+++ RifasMAUIApp/Services/RifasService.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using RifasMAUIApp.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace RifasMAUIApp.Services
+{
+    public class RifasService
+    {
+        HttpClient client;
+
+        public RifasService()
+        {
+            client.BaseAddress = new Uri("https://rifas.sistemas19.com/");
+        }
+
+        public async Task<IEnumerable<BoletoDTO>> GetAll()
+        {
+            var response = await client.GetAsync("api/rifas/vendidos");
+            response.EnsureSuccessStatusCode();//Verificar que regreso un 200
+
+            var json = await response.Content.ReadAsStringAsync();
+            var datos = JsonConvert.DeserializeObject<IEnumerable<BoletoDTO>>(json);
+            return datos;
+
+        }
+
+        public void Post()
+        {
+
+        }
+
+        public void Put()
+        {
+
+        }
+
+        //Se tiene que cancelar
+        public void Delete()
+        {
+
+        }
+    }
+}
